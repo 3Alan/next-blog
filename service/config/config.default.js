868ea40507f30980398a1cc8adce6f719b8528e0
3Alan <1540703192@qq.com>
2020-05-11 13:55:05 +0800

    production 配置

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -35,7 +35,7 @@ module.exports = appInfo => {
       // port
       port: '3306',
       // username
-      user: 'root',
+      user: 'alan',
       // password
       password: 'wp.666',
       // database
